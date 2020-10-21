57ae09ddae80d2acbbadcda91f83ed2d9ff79d79
mirror <mirror010@hotmail.com>
2020-10-21 22:05:40 +0800

    底部菜单手机组件

---
+++ mirrorui/js/mnavmenu.js
@@ -0,0 +1,45 @@
+﻿/*
+ * 底部导航菜单,用于手机
+ * let menu = mnavmenu(domId,menuItemClickEvent);
+ */
+((win) => {
+    // 帮助函数
+    const M = win.$ui;
+    // 传入菜单nav标记的id,生成菜单 menuItemClickE(menuItemDom):菜单项点击事件
+    let mnavMenu = (menuboxId, menuItemClickE) => {
+        // 菜单navDom
+        let self = {};
+        let menuDom = document.getElementById(menuboxId);
+        bindEvent_menuGroup(menuDom);
+        bindEvent_menuItem(menuDom, menuItemClickE);
+        return self;
+    };
+
+    // 菜单组收起和展开
+    let bindEvent_menuGroup = (menuDom) => {
+        M(menuDom).find('.mnavmenu-label').each((item, i) => {
+            item.onclick = () => {
+                let itemBoxCls = '.mnavmenu-itembox';
+                let menuItemIsClose = M(item).prev(itemBoxCls).hasClass('close');
+                M(menuDom).find(itemBoxCls).addClass('close');
+                if (menuItemIsClose == true) {
+                    M(item).prev(itemBoxCls).removeClass('close');
+                }
+            }
+        })
+    };
+
+    // 菜单项点击
+    let bindEvent_menuItem = (menuDom, menuItemClickE) => {
+        M(menuDom).find('.mnavmenu-item').each((item, index) => {
+            item.onclick = () => {
+                M(item).parent('.mnavmenu').find('.mnavmenu-itembox').addClass('close');
+                if (typeof menuItemClickE == 'function')
+                    menuItemClickE(item, index);
+            }
+        });
+    };
+
+    // window上的引用名字
+    win.mnavmenu = mnavMenu;
+})(window);
